32ea2d9740a5fc3dc8efb0a15eb5f2db218a6709
Carlos Hernandez <carloshdmn@gmail.com>
2017-11-28 00:37:52 +0100

    Increase scopes

--- js/app.js
+++ js/app.js
@@ -36,7 +36,7 @@
 		// Spotify client
 		SpotifyProvider.setClientId(config.spotify.clientId);
 		SpotifyProvider.setRedirectUri(config.spotify.callback);
-		SpotifyProvider.setScope('user-read-private playlist-read-private playlist-modify-private playlist-modify-public');
+		SpotifyProvider.setScope('user-read-private playlist-read-private playlist-modify-private playlist-modify-public user-top-read user-read-recently-played user-library-read user-library-modify user-follow-read user-follow-modify');
 		// If you already have an auth token
 		SpotifyProvider.setAuthToken(config.spotify.token);
 
